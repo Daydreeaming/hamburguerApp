e6ebd37d70d4498fcd91400368477cd5406f1ae2
Lucas Dantas <lucascamposdantas@hotmail.com>
2021-07-27 15:31:12 -0300

    Começo de projeto - Hamburguer APP

---
+++ lib/hamburguerApp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HamburguerApp extends StatefulWidget {
+  @override
+  _HamburguerAppState createState() => _HamburguerAppState();
+}
+
+class _HamburguerAppState extends State<HamburguerApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
